5dcc24960afa73a65be9a42668fd0a561782a85f
Rob Husband <rob.a.husband@gmail.com>
2024-02-09 23:46:07 +0000

    Added guard helper class

---
+++ Source/MindstormsSharp.Core/Helpers/Guard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MindstormsSharp.Core.Helpers
+{
+    public static class Guard
+    {
+        public static TObject AgainstNull<TObject>(TObject obj, string parameterName) 
+            => obj != null ? obj : throw new ArgumentNullException(parameterName);
+
+        public static string AgainstNullAndEmpty(string value, string parameterName) 
+            => !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentNullException(parameterName);
+    }
+}
---
+++ Tests/MindstormsSharp.Core.Tests/Helpers/GuardTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using MindstormsSharp.Core.Helpers;
+
+namespace MindstormsSharp.Core.Tests.Helpers
+{
+    [TestFixture]
+    public class GuardTests
+    {
+        #region Guard.AgainstNull Tests
+
+        [Test]
+        public void GivenANullObject_WhenGuardingAgainstNull_ThenExceptionIsThrown()
+        {
+            var testObject = new TestObject();
+
+            Assert.That(() => Guard.AgainstNull<TestObject>(null!, nameof(testObject)),
+                Throws.ArgumentNullException.With.Property("ParamName").EqualTo("testObject"));
+        }
+
+        [Test]
+        public void GivenAnObject_WhenGuardingAgainstNull_ThenNoExceptionThrown()
+        {
+            var testObject = new TestObject();
+
+            Assert.That(() => Guard.AgainstNull(testObject, nameof(testObject)), Throws.Nothing);
+        }
+
+        #endregion
+
+        #region Guard.AgainstNullAndEmpty (String) Tests
+
+        [TestCaseSource(nameof(NullStringCases))]
+        public void GivenAnInvalidStringValue_WhenGuardingAgainstNullAndEmpty_ThenExceptionIsThrown(string value, string paramName)
+        {
+            Assert.That(() => Guard.AgainstNullAndEmpty(value, paramName), 
+                Throws.ArgumentNullException.With.Property("ParamName").EqualTo(paramName));
+        }
+
+        private static IEnumerable<TestCaseData> NullStringCases()
+        {
+            yield return new TestCaseData(null, "nullValue").SetName("Null string value test");
+            yield return new TestCaseData("", "emptyValue").SetName("Empty string value test");
+            yield return new TestCaseData("     ", "whitespaceValue").SetName("Whitespace string value test");
+        }
+
+        [Test]
+        public void GivenAValidStringValue_WhenGuardingAgainstNullAndEmpty_ThenNoExceptionIsThrown()
+        {
+            Assert.That(() => Guard.AgainstNullAndEmpty("some string value", "some param name"), Throws.Nothing);
+        }
+
+        #endregion
+
+        #region Test Helpers
+
+        private class TestObject
+        {
+            public bool BooleanProperty => false;
+            public string StringProperty => "some string value";
+        }
+
+        #endregion
+    }
+}
